982f8bc36ced15ad9ba47d47a3a33fc5f3ca8684
Arlan Dantas <lanfsa@hotmail.com>
2015-11-11 22:16:08 -0300

    Apenas criando o commit inicial!

---
+++ preguica.js
@@ -0,0 +1,10 @@
+var chars = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
+var ret = "";
+for(var i = 0; i < chars.length; i++) {
+    var c = chars.charAt(i).toLowerCase();
+    if(ret != "") {
+        ret += ",\n";
+    }
+    ret += '{\t"caractere": "'+c+'",\n\t"braille": ""\n}';
+}
+console.log(ret);
---
+++ prototipo01.js
@@ -0,0 +1,435 @@
+var caracteres = [
+    {
+        "caractere": "a",
+    	"braille": "100000"
+    },
+    {
+        "caractere": "b",
+    	"braille": "110000"
+    },
+    {
+        "caractere": "c",
+    	"braille": "100100"
+    },
+    {
+        "caractere": "d",
+    	"braille": "100110"
+    },
+    {
+        "caractere": "e",
+    	"braille": "100010"
+    },
+    {
+        "caractere": "f",
+    	"braille": "110100"
+    },
+    {
+        "caractere": "g",
+    	"braille": "110110"
+    },
+    {
+        "caractere": "h",
+    	"braille": "110010"
+    },
+    {
+        "caractere": "i",
+    	"braille": "010100"
+    },
+    {
+        "caractere": "j",
+    	"braille": "010110"
+    },
+    {
+        "caractere": "k",
+    	"braille": "101000"
+    },
+    {
+        "caractere": "l",
+    	"braille": "111000"
+    },
+    {
+        "caractere": "m",
+    	"braille": "101100"
+    },
+    {
+        "caractere": "n",
+    	"braille": "101110"
+    },
+    {
+        "caractere": "o",
+    	"braille": "101010"
+    },
+    {
+        "caractere": "p",
+    	"braille": "111100"
+    },
+    {
+        "caractere": "q",
+    	"braille": "111110"
+    },
+    {
+        "caractere": "r",
+    	"braille": "111010"
+    },
+    {
+        "caractere": "s",
+    	"braille": "011100"
+    },
+    {
+        "caractere": "t",
+    	"braille": "011110"
+    },
+    {
+        "caractere": "u",
+    	"braille": "101001"
+    },
+    {
+        "caractere": "v",
+    	"braille": "111001"
+    },
+    {
+        "caractere": "x",
+    	"braille": "101101"
+    },
+    {
+        "caractere": "w",
+    	"braille": "010111"
+    },
+    {
+        "caractere": "y",
+    	"braille": "101111"
+    },
+    {
+        "caractere": "z",
+    	"braille": "101011"
+    },
+    {
+        "caractere": "ç",
+    	"braille": "111101"
+    },
+    {
+        "caractere": "ã",
+    	"braille": "001110"
+    },
+    {
+        "caractere": "õ",
+    	"braille": "010101"
+    },
+    {
+        "caractere": "â",
+    	"braille": "100001"
+    },
+    {
+        "caractere": "ê",
+    	"braille": "110001"
+    },
+    {
+        "caractere": "ï",
+    	"braille": "110111"
+    },
+    {
+        "caractere": "ô",
+    	"braille": "100111"
+    },
+    {
+        "caractere": "ü",
+    	"braille": "110011"
+    },
+    {
+        "caractere": "á",
+    	"braille": "111011"
+    },
+    {
+        "caractere": "é",
+    	"braille": "111111"
+    },
+    {
+        "caractere": "í",
+    	"braille": "001100"
+    },
+    {
+        "caractere": "ó",
+    	"braille": "001101"
+    },
+    {
+        "caractere": "ú",
+    	"braille": "011111"
+    },
+    {
+        "caractere": "à",
+    	"braille": "110101"
+    },
+    {
+        "caractere": "è",
+    	"braille": "011101"
+    },
+    {
+        "caractere": "ì",
+    	"braille": "100101"
+    },
+    {
+        "caractere": "ò",
+    	"braille": "010111"
+    },
+    {
+        "caractere": "ù",
+    	"braille": "100011"
+    },
+    {
+        "caractere": "0",
+    	"braille": "010110"
+    },
+    {
+        "caractere": "1",
+    	"braille": "100000"
+    },
+    {
+        "caractere": "2",
+    	"braille": "110000"
+    },
+    {
+        "caractere": "3",
+    	"braille": "100100"
+    },
+    {
+        "caractere": "4",
+    	"braille": "100110"
+    },
+    {
+        "caractere": "5",
+    	"braille": "100010"
+    },
+    {
+        "caractere": "6",
+    	"braille": "110100"
+    },
+    {
+        "caractere": "7",
+    	"braille": "110110"
+    },
+    {
+        "caractere": "8",
+    	"braille": "110010"
+    },
+    {
+        "caractere": "9",
+    	"braille": "010100"
+    },
+    {
+        "caractere": "ª",
+    	"braille": "100001100000"
+    },
+    {
+        "caractere": "º",
+    	"braille": "100001101010"
+    },
+    {
+        "caractere": ",",
+    	"braille": "010000"
+    },
+    {
+        "caractere": ";",
+    	"braille": "011000"
+    },
+    {
+        "caractere": ":",
+    	"braille": "010010"
+    },
+    {
+        "caractere": ".",
+    	"braille": "001000"
+    },
+    {
+        "caractere": "'",
+    	"braille": "001000"
+    },
+    {
+        "caractere": "?",
+    	"braille": "010001"
+    },
+    {
+        "caractere": "!",
+    	"braille": "011010"
+    },
+    {
+        "caractere": "(",
+    	"braille": "110001001000"
+    },
+    {
+        "caractere": ")",
+    	"braille": "000001001110"
+    },
+    {
+        "caractere": "«",
+    	"braille": "011001"
+    },
+    {
+        "caractere": "»",
+    	"braille": "011001"
+    },
+    {
+        "caractere": "-",
+    	"braille": "001001"
+    },
+    {
+        "caractere": "*",
+    	"braille": "001010"
+    },
+    {
+        "caractere": "•",
+    	"braille": "010101101010"
+    },
+    {
+        "caractere": "→",
+    	"braille": "010010101010"
+    },
+    {
+        "caractere": "←",
+    	"braille": "010101010010"
+    },
+    {
+        "caractere": "↔",
+    	"braille": "010101010010101010"
+    },
+    {
+        "caractere": "€",
+    	"braille": "000100100010"
+    },
+    {
+        "caractere": "$",
+    	"braille": "000011"
+    },
+    {
+        "caractere": "%",
+    	"braille": "000111001011"
+    },
+    {
+        "caractere": "‰",
+    	"braille": "000111001011001011"
+    },
+    {
+        "caractere": "§",
+    	"braille": "011100011100"
+    },
+    {
+        "caractere": "+",
+    	"braille": "011010"
+    },
+    {
+        "caractere": "-",
+    	"braille": "001001"
+    },
+    {
+        "caractere": "X",
+    	"braille": "011001"
+    },
+    {
+        "caractere": "=",
+    	"braille": "011011"
+    },
+    {
+        "caractere": ">",
+    	"braille": "101010"
+    },
+    {
+        "caractere": "<",
+    	"braille": "010101"
+    },
+    {
+        "caractere": "º",
+    	"braille": "001011"
+    },
+    {
+        "caractere": "’",
+    	"braille": "110011"
+    },
+    {
+        "caractere": "´",
+    	"braille": "110011"
+    },
+    {
+        "caractere": " ",
+    	"braille": "000000"
+    }
+];
+
+var funcoes = [
+    "001111",               // Número
+    "000101",               // Maiúscula
+    "000101000101",         // Palavra Maiúscula
+    "010010000101000101",   // Sequencia Maiúscula
+    "100001",               // Expoente / Índice Superior
+    "001100",               // Índice Inferior
+    "001010",               // Negrito / Itálico / Sublinhado
+    "000010010010",         // Transpaginação
+    "",//
+    "",//
+    "",//
+    ""//
+];
+
+imprimir(process.argv[2]);
+
+function imprimir(texto) {
+    var texto_braille = "";
+    for (var i = 0; i < texto.length; i++) {
+        var atual = texto.charAt(i);
+        var anterior = i > 0 ? texto.charAt(i-1) : null;
+        var caractere = getBraille(atual);
+
+        if(isInt(atual) && !isInt(anterior)) {
+            texto_braille += funcoes[0];
+        }
+
+        if(isUpper(atual)) {
+            var palavra_maiusc = true;
+            var inicial = i;
+
+            for (var j = inicial; j >= 0; j--) {
+                if(texto.charAt(j-1) == " " || texto.charAt(j-1) == null || j == 0) {
+                    inicial = j;
+                    break;
+                }
+            }
+            for (var j = inicial; j < texto.length; j++) {
+                if(texto.charAt(j) == " ") {
+                    break;
+                }
+                if( !isUpper(texto.charAt(j)) ) {
+                    palavra_maiusc = false;
+                }
+            }
+
+            if((anterior == " " || anterior == null) && palavra_maiusc) {
+                texto_braille += funcoes[2];
+            }
+            if (!palavra_maiusc) {
+                texto_braille += funcoes[1];
+            }
+        }
+
+        texto_braille += caractere.braille;
+    }
+    for (var i = 0; i < texto_braille.length; i+=6) {
+        console.log(texto_braille.substr(i, 6));
+    }
+}
+
+function isUpper(chr) {
+    return chr.length == 1 && /[A-Z]|[\u0080-\u024F]/.test(chr) && chr === chr.toUpperCase();
+}
+
+function isInt(value) {
+    return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
+}
+
+function getBraille(char) {
+    var ret = null;
+    for (var i = 0; i < caracteres.length; i++) {
+        if(caracteres[i].caractere == char.toLowerCase()) {
+            ret = caracteres[i];
+            break;
+        }
+    }
+    return ret;
+}
